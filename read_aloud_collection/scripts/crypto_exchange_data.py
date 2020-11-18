11e123a250472f338c523037f035a2a8fd47941b
tfari <28794192+tfari@users.noreply.github.com>
2020-11-18 19:01:40 -0300

    Fix crypto_exchange.py
    Adding header information to the GET request did the trick.

--- read_aloud_collection/scripts/crypto_exchange_data.py
+++ read_aloud_collection/scripts/crypto_exchange_data.py
@@ -12,14 +12,17 @@ def crypto_exchange(symbol_1, symbol_2):
     :param symbol_2: str, monetary(or crypto) symbol
     :return: str, exchange rate of one symbol_1 in symbol_2 currency
     """
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0'
+    }
     url = 'https://api.cryptonator.com/api/ticker/%s-%s' % (symbol_1.lower(), symbol_2.lower())
-    rh = RequestHandler([url], RequestData(GET), RequestErrorData(allow_errors=True))
+    rh = RequestHandler([url], RequestData(GET, headers=headers), RequestErrorData(allow_errors=True))
     rh.run()
     if rh.errors:
         print("[!] Crypto exchange returned error: %s" % rh.errors)
         return 'Crypto exchange returned error: %s' % rh.errors[0]['response'].status_code
 
-    return 'One %s is valued %s %s.' % (symbol_1, '{0:.10}'.format(rh.responses[0].json()['ticker']['price']), symbol_2)
+    return 'One %s is valued %s %s.' % (symbol_1, '%.1f' % float(rh.responses[0].json()['ticker']['price']), symbol_2)
 
 
 if __name__ == "__main__":
